50c5f4481b001cd4cccdd1966895d4f1acf7309f
Ilya Domanov <domanorila@gmail.com>
2023-07-03 21:39:10 +0700

    added info about directions

--- snake.py
+++ snake.py
@@ -1,6 +1,21 @@
+import enum
 import pygame
 
+
+direction = enum.Enum(
+    value='direction',
+    names='up down left right',
+)
+
 class Snake:
+
+    shifts_in_directions = {
+        direction.up:   (0, -1),
+        direction.down: (0, 1),
+        direction.left: (-1, 0),
+        direction.right:(1, 0),
+    }
+
     def __init__(self):
         self.body = []
         self.direction = 'right'
