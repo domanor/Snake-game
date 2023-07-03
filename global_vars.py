7930d042e65896128b5b7ddc01df849372aa267d
Ilya Domanov <domanorila@gmail.com>
2023-07-03 21:17:14 +0700

    Update global_vars.py

---
+++ global_vars.py
@@ -0,0 +1,17 @@
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+BACKGROUND_COLOR = (110, 170, 210)  #light blue
+APPLE_COLOR = (255, 0, 0)           #red
+SNAKE_COLOR = (0, 255, 0)           #green
+
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+
+#cell size must be a divider of the width and height of the window
+CELL_SIZE = 30
+
+GRID_WIDTH = WINDOW_WIDTH//CELL_SIZE
+GRID_HEIGHT = WINDOW_WIDTH//CELL_SIZE
+FPS = 15
