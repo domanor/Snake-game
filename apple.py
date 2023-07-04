8d6dc332b08d4d1b75e3ea1a7fb4b76df54f013f
Ilya Domanov <domanorila@gmail.com>
2023-07-04 12:04:46 +0700

    Update apple.py

---
+++ apple.py
@@ -0,0 +1,36 @@
+import random
+from pygame import draw
+from global_vars import *
+
+
+class Apple:
+
+    def __init__(self):
+        self.generate_new_position(
+            exception_positions=[(GRID_WIDTH//2, GRID_HEIGHT//2)]
+        )
+    
+    def generate_new_position(self, exception_positions=[]):
+        generate=True
+        while generate:
+            x_new_pos = random.randint(0, GRID_WIDTH-1)
+            y_new_pos = random.randint(0, GRID_HEIGHT-1)
+
+            if (x_new_pos, y_new_pos) not in exception_positions:
+                self.position = (x_new_pos, y_new_pos)
+                generate=False
+
+    def draw(self, surface):
+        x_window_pos = self.position[0]*CELL_SIZE
+        y_window_pos = self.position[1]*CELL_SIZE
+
+        draw.rect(
+            surface,
+            APPLE_COLOR,
+            (
+                x_window_pos,
+                y_window_pos,
+                CELL_SIZE,
+                CELL_SIZE,
+            )
+        )
