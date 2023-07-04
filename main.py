bad9ef2de4490369cdcfba312b578a29b706ec52
Ilya Domanov <domanorila@gmail.com>
2023-07-04 17:53:17 +0700

    added head snake and apple in window

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 import pygame
 from global_vars import *
 from snake import *
+import food
+
+
+snake = None
+apple = None
+running = None
+
+def loop(window):
+    global running, snake, apple
+
+    snake.move()
+    
+    window.fill(BACKGROUND_COLOR)
+    snake.draw(window)
+    apple.draw(window)
+    pygame.display.update()
+
 
 def main(window):
+    global running, snake, apple
 
     pygame.init()
     pygame.display.set_caption('game SNAKE')
 
     snake = Snake()
+    apple = food.Apple()
+    clock = pygame.time.Clock()
     
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-    
+        loop(window)
+
+        clock.tick(FPS)    
     pygame.quit()
 
 
