257396b87a082594162e86ef3c2d365e58c5bd9e
Ilya Domanov <domanorila@gmail.com>
2023-07-04 17:36:50 +0700

    added game cycle in main.py

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import pygame
+from global_vars import *
+from snake import *
 
 def main(window):
 
     pygame.init()
+    pygame.display.set_caption('game SNAKE')
+
+    snake = Snake()
+    
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    
+    pygame.quit()
 
 
 
 if __name__ == '__main__':
-    my_window = pygame.display.set_mode((300, 300))
+    my_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
     main(my_window)
